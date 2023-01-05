3b60972dcac2749d551d17c399e0f01379a4814f
Shaojiasong <lesonky@163.com>
2023-01-05 11:56:07 +0800

    feat: add gittalk

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,9 @@
 import Theme from 'vitepress/theme'
 import './styles/vars.scss'
 import './styles/style.scss'
+import MyLayout from './MyLayout.vue'
 
 export default {
   ...Theme,
+  Layout: MyLayout
 }
