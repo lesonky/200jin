3f3e29b5c610cc9f24f8c1da4f6bc8ba56d896be
shaojiasong <lesonky@163.com>
2023-03-22 10:07:48 +0800

    remove 备案

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
     ],
     socialLinks: [{ icon: 'github', link: 'https://github.com/lesonky' }],
     footer: {
-      message: '<a href="https://beian.miit.gov.cn/">苏ICP备2020067590号</a>',
-      copyright: 'Copyright © 2022-12-06 ～ present Wukoon',
+      message: '不畏浮云遮望眼，自缘身在最高层',
+      copyright: 'Copyright © 2022-12-06 - present Wukoon',
     },
     sidebar: buildSidebar(),
   },
