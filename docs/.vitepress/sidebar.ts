861f585a9a0173bdab7a9483a903fef52deb43f0
shaojiasong <lesonky@163.com>
2023-08-03 10:11:47 +0800

    add doc

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,28 +1,31 @@
 export default function buildSidebar() {
   return {
-    '/daily/': [
+    "/daily/": [
       {
-        text: '意识流',
-        collapsible: true,
-        items: [{ text: '关于这里', link: '/daily/' }],
-      },
-      {
-        text: '搭建博客',
+        text: "随笔",
         collapsible: true,
         items: [
-          { text: 'Vitepress 搭建博客', link: '/daily/build_vitepress_blog' },
-          { text: 'Github Pages 发布博客', link: '/daily/publish_blog' },
-          { text: 'Github Actions 自动化部署', link: '/daily/ci_blog' },
-          { text: '添加gitalk支持', link: '/daily/gitalk_blog' },
+          { text: "关于这里", link: "/daily/" },
+          { text: "Hello World 随想", link: "/daily/hello_world" },
         ],
       },
       {
-        text: 'GWS 注册和购买',
+        text: "搭建博客",
         collapsible: true,
         items: [
-          { text: '新手指南', link: '/daily/gws/buy_gws' },
-          { text: '如何购买域名', link: '/daily/gws/reg_domain' },
-          { text: '如何通过 GWS 域名认证', link: '/daily/gws/dns_setting' },
+          { text: "Vitepress 搭建博客", link: "/daily/blog/build_vitepress_blog" },
+          { text: "Github Pages 发布博客", link: "/daily/blog/publish_blog" },
+          { text: "Github Actions 自动化部署", link: "/daily/blog/ci_blog" },
+          { text: "添加gitalk支持", link: "/daily/blog/gitalk_blog" },
+        ],
+      },
+      {
+        text: "GWS 注册和购买",
+        collapsible: true,
+        items: [
+          { text: "新手指南", link: "/daily/gws/buy_gws" },
+          { text: "如何购买域名", link: "/daily/gws/reg_domain" },
+          { text: "如何通过 GWS 域名认证", link: "/daily/gws/dns_setting" },
         ],
       },
     ],
